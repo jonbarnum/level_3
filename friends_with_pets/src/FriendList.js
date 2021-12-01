8bc74174cb9bc1c2b4a79491433eca8ff841284b
Jonathan Barnum <jonbarnum07@gmail.com>
2021-12-01 16:09:42 -0700

    basic css added

--- friends_with_pets/src/FriendList.js
+++ friends_with_pets/src/FriendList.js
@@ -4,9 +4,9 @@ import Pet from "./Pet";
 function FriendList(props){
     return(
         <div>
-            <h1>{`Name: ${props.friend.name}`}</h1>
-            <h2>{`Age: ${props.friend.age}`}</h2>
-            <h3>
+            <h1 className={'friendName'}>{`Name: ${props.friend.name}`}</h1>
+            <h2 className={'friendAge'}>{`Age: ${props.friend.age}`}</h2>
+            <h3 className={'pets'}>
                 {
                     props.friend.pets.map((pet) => (
                         <Pet key={pet.name} pet={pet}/>
--- friends_with_pets/src/Pet.js
+++ friends_with_pets/src/Pet.js
@@ -1,16 +1,10 @@
 import React from "react";
 
-// function Pet({
-//     pet,
-//     color
-// }){
-//     return color + pet.name
-// }
 function Pet(props){
     return(
         <div>
-            <h3>{`Pet Name: ${props.pet.name}`}</h3>
-            <h3>{`Pet Breed: ${props.pet.breed}`}</h3>
+            <h3 className={'petName'}>{`Pet Name: ${props.pet.name}`}</h3>
+            <h3 className={'petBreed'}>{`Pet Breed: ${props.pet.breed}`}</h3>
         </div>
     )
 }
